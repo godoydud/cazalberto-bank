7903a9c8ad72f1ad3a6407da8e71759b45bd1874
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2024-02-24 13:50:15 -0400

    feat: adicionado mensagem do coordenador

--- coordenador/coordenador.py
+++ coordenador/coordenador.py
@@ -13,39 +13,56 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Associe o socket ao endereço e à porta
 server_socket.bind((HOST, PORT))
 
-# fila de mensagens 
+# fila de mensagens
 fila_de_mensagens = queue.Queue()
 
 
 # enviar mensagem de operações de debito ao shard_a na porta 6000
-def enviar_mensagem_debito():
+def enviar_mensagem_debito(message):
     shard_a_ip = 'shard_b'
     shard_a_port = 7000
-    message = "operacao de debito"
+    message = f"operacao de debito: Cliente {message['conta_cliente']} Valor: {message['valor_operacao']}"
     server_socket.sendto(message.encode(), (shard_a_ip, shard_a_port))
     print("mensagem de debito enviada")
 
 # enviar mensagem de operações de credito ao shard_b na porta 7000
-def enviar_mensagem_credito():
+
+
+def enviar_mensagem_credito(message):
     shard_b_ip = 'shard_a'
     shard_b_port = 6000
-    message = "operacao de credito"
+    message = f"operacao de credito: Cliente {message['conta_cliente']} Valor: {message['valor_operacao']}"
     server_socket.sendto(message.encode(), (shard_b_ip, shard_b_port))
     print("mensagem de credito enviada")
 
 
 print("Servidor UDP esperando conexão na porta", PORT)
 
+
 def processar_mensagem(mensagem):
     commit = ""
     try:
         if mensagem["tipo_mensagem"] == "operacao":
             if mensagem["detalhes"]["tipo"] == "C":
-                enviar_mensagem_credito()
-                server_socket.sendto("mensagem de credito processada".encode(), address)
+                enviar_mensagem_credito(mensagem["detalhes"])
+                mensagem = {
+                    "tipo_mensagem": "commit",
+                    "detalhes": {
+                        "mensagem": "OK",
+                    }
+                }
+                server_socket.sendto(
+                    str(json.dumps(mensagem)).encode(), address)
             elif mensagem["detalhes"]["tipo"] == "D":
-                enviar_mensagem_debito()
-                server_socket.sendto("mensagem de debito processada".encode(), address)
+                enviar_mensagem_debito(mensagem["detalhes"])
+                mensagem = {
+                    "tipo_mensagem": "commit",
+                    "detalhes": {
+                        "mensagem": "OK",
+                    }
+                }
+                server_socket.sendto(
+                    str(json.dumps(mensagem)).encode(), address)
             print("OPERACAO RECEBIDA:", mensagem)
         elif mensagem["tipo_mensagem"] == "commit":
             print("COMMIT RECEBIDO:", mensagem)
@@ -54,13 +71,14 @@ def processar_mensagem(mensagem):
         print("mensagem inválida recebida")
         pass
 
+
 while True:
     data, address = server_socket.recvfrom(BUFFER_SIZE)
 
     # Converta a mensagem recebida para uma string e imprima
     print("Mensagem recebida:", data.decode())
     mensagem_recebida = json.loads(data.decode())
-    
+
     # Adiciona a mensagem à fila
     fila_de_mensagens.put(mensagem_recebida)
 
