8f68f713fee88260cef292d3b1b2d7eb44bfb9fc
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2024-02-24 13:34:44 -0400

    feat: adicionando fila para tratar

--- coordenador/coordenador.py
+++ coordenador/coordenador.py
@@ -1,5 +1,6 @@
 import socket
 import json
+import queue
 
 # Defina a porta e o tamanho do buffer
 PORT = 5000
@@ -11,6 +12,10 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Associe o socket ao endereço e à porta
 server_socket.bind((HOST, PORT))
+
+# fila de mensagens 
+fila_de_mensagens = queue.Queue()
+
 
 # enviar mensagem de operações de debito ao shard_a na porta 6000
 def enviar_mensagem_debito():
@@ -31,27 +36,35 @@ def enviar_mensagem_credito():
 
 print("Servidor UDP esperando conexão na porta", PORT)
 
+def processar_mensagem(mensagem):
+    commit = ""
+    try:
+        if mensagem["tipo_mensagem"] == "operacao":
+            if mensagem["detalhes"]["tipo"] == "C":
+                enviar_mensagem_credito()
+                server_socket.sendto("mensagem de credito processada".encode(), address)
+            elif mensagem["detalhes"]["tipo"] == "D":
+                enviar_mensagem_debito()
+                server_socket.sendto("mensagem de debito processada".encode(), address)
+            print("OPERACAO RECEBIDA:", mensagem)
+        elif mensagem["tipo_mensagem"] == "commit":
+            print("COMMIT RECEBIDO:", mensagem)
+            commit = mensagem
+    except:
+        print("mensagem inválida recebida")
+        pass
+
 while True:
     data, address = server_socket.recvfrom(BUFFER_SIZE)
 
     # Converta a mensagem recebida para uma string e imprima
     print("Mensagem recebida:", data.decode())
     mensagem_recebida = json.loads(data.decode())
-    commit = ""
-    try:
-        if mensagem_recebida["tipo_mensagem"] == "operacao":
-            if mensagem_recebida["detalhes"]["tipo"] == "C":
-                enviar_mensagem_credito()
-                server_socket.sendto("mensagem de credito processada".encode(), address)
-            elif mensagem_recebida["detalhes"]["tipo"] == "D":
-                enviar_mensagem_debito()
-                server_socket.sendto("mensagem de debito processada".encode(), address)
-            print("OPERACAO RECEBIDA:", mensagem_recebida)
-        elif mensagem_recebida["tipo_mensagem"] == "commit":
-            print("COMMIT RECEBIDO:", mensagem_recebida)
-            commit = mensagem_recebida
-    except:
-        print("mensagem inválida recebida")
-        pass
     
-    # Envie uma resposta de volta para o cliente
+    # Adiciona a mensagem à fila
+    fila_de_mensagens.put(mensagem_recebida)
+
+    # Processa todas as mensagens na fila
+    while not fila_de_mensagens.empty():
+        mensagem = fila_de_mensagens.get()
+        processar_mensagem(mensagem)
