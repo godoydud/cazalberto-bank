d09b7226a8601233e0986f3365ebfcc2044b932a
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2024-02-24 12:06:02 -0400

    feat: conexão com o UDP

--- coordenador/coordenador.py
+++ coordenador/coordenador.py
@@ -1,1 +1,18 @@
-print("Hello from coordenador")
+import socket
+
+HOST = '0.0.0.0'
+PORT = 5000
+BUFFER_SIZE = 50
+
+def main():
+    # create a socket UDP to listen and print all message received
+    print("Hello from coordenador")
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.bind((HOST, PORT))
+        while True:
+            data, addr = s.recvfrom(BUFFER_SIZE)
+            print(f"Received {data.decode()} from {addr}")
+
+if __name__ == "__main__":
+    main()
+    
