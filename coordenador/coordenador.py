58ae7d86316d9cb47b427a5db3b40854e9213980
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2024-02-24 12:44:01 -0400

    fix: ajuste nas mensagens

--- coordenador/coordenador.py
+++ coordenador/coordenador.py
@@ -1,18 +1,77 @@
 import socket
 
-HOST = '0.0.0.0'
+HOST = "0.0.0.0"
 PORT = 5000
-BUFFER_SIZE = 1024  
+BUFFER_SIZE = 1024
+
+
+def enviar_mensagem_credito():
+    # Endereço IP e porta do servidor UDP
+    server_ip = "shard_a"
+    server_port = 6000
+
+    # Cria um socket UDP
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Mensagem a ser enviada ao servidor
+    message = "Olá, shard_a!"
+
+    # Envia a mensagem ao servidor
+    client_socket.sendto(message.encode(), (server_ip, server_port))
+
+    # Recebe a resposta do servidor
+    response, server_address = client_socket.recvfrom(1024)
+
+    # Exibe a resposta do servidor
+    print("Resposta do shard_a:", response.decode())
+
+    # Fecha o socket
+    client_socket.close()
+
+
+def enviar_mensagem_debito():
+    # Endereço IP e porta do servidor UDP
+    server_ip = "shard_b"
+    server_port = 7000
+
+    # Cria um socket UDP
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Mensagem a ser enviada ao servidor
+    message = "Olá, shard_a!"
+
+    # Envia a mensagem ao servidor
+    client_socket.sendto(message.encode(), (server_ip, server_port))
+
+    # Recebe a resposta do servidor
+    response, server_address = client_socket.recvfrom(1024)
+
+    # Exibe a resposta do servidor
+    print("Resposta do shard_a:", response.decode())
+
+    # Fecha o socket
+    client_socket.close()
+
 
 # create a socket UDP to wait listen all clients in the network
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind((HOST, PORT))
 
-print('Servidor UDP esperando conexão na porta', PORT)
+print("Servidor UDP esperando conexão na porta", PORT)
 
 while True:
-    message, client_address = server_socket.recvfrom(BUFFER_SIZE)
-    print('Mensagem recebida do cliente', client_address, ':', message.decode())
-    server_socket.sendto('Olá, cliente!'.encode(), client_address)
-    print('Resposta enviada ao cliente', client_address)
+    bytesAddressPair = server_socket.recvfrom(BUFFER_SIZE)
+    
+    print(bytesAddressPair)
 
+    address = bytesAddressPair[1]
+
+    clientMsg = "Message from Client:{}".format(message)
+    clientIP = "Client IP Address:{}".format(address)
+
+    print(clientMsg)
+    print(clientIP)
+
+    # Sending a reply to client
+
+    server_socket.sendto("teste".encode(), address)
--- shard_a/shard_a.py
+++ shard_a/shard_a.py
@@ -1,1 +1,20 @@
-print("Hello from shard_a")
+import socket
+
+HOST = '0.0.0.0'
+PORT = 6000
+BUFFER_SIZE = 1024
+
+# create a socket UDP to wait listen all clients in the network
+print("shard_a recebendo operações de crédito instanciado")
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind((HOST, PORT))
+
+print('Servidor UDP esperando conexão na porta', PORT)
+
+while True:
+    message, client_address = server_socket.recvfrom(BUFFER_SIZE)
+    print('Mensagem recebida do cliente',
+          client_address, ':', message.decode())
+    server_socket.sendto('Olá, credito!'.encode(), client_address)
+    print('Resposta enviada ao cliente', client_address)
