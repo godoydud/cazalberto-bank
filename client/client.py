748188f41bbf7666191b1ea963bfcb915ce11f5c
CIS <eduardo.fuso@sesims.com.br>
2024-02-24 12:13:58 -0400

    Feat: Implementado socket UDP em client

--- client/client.py
+++ client/client.py
@@ -1,1 +1,23 @@
-print("Hello from client")
+import socket
+
+# Endereço IP e porta do servidor UDP
+server_ip = 'coordenador'
+server_port = 5000
+
+# Cria um socket UDP
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Mensagem a ser enviada ao servidor
+message = 'Olá, servidor!'
+
+# Envia a mensagem ao servidor
+client_socket.sendto(message.encode(), (server_ip, server_port))
+
+# Recebe a resposta do servidor
+response, server_address = client_socket.recvfrom(1024)
+
+# Exibe a resposta do servidor
+print('Resposta do servidor:', response.decode())
+
+# Fecha o socket
+client_socket.close()
